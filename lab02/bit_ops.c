7c27a517e4656d88c19e2a145b1c25390e518a17
Pluto-yubin <1113940328@qq.com>
2022-03-30 15:19:06 +0800

    pass lab2 loca testcase

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
 #include "bit_ops.h"
+#include <limits.h>
+#include <stdio.h>
 // add something
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns
-    // 0 or 1)
-    return -1;
+    unsigned n)
+{
+    unsigned int mask = 1;
+    mask = mask << n;
+    return (x & mask) >> n;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
+void set_bit(unsigned* x,
+    unsigned n,
+    unsigned v)
+{
+    // get the mask that all bits are 1 expect the nth one
+    // the nth one is v
+    int mask = INT_MAX - ((v ^ 1) << n);
+    *x = *x & mask;
+    *x = *x | (v << n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+void flip_bit(unsigned* x,
+    unsigned n)
+{
+    set_bit(x, n, get_bit(*x, n) ^ 1);
 }
-
