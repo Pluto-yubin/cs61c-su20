180c56069c9471f14e79048ba6be1fc012088861
Pluto-yubin <1113940328@qq.com>
2022-04-03 23:00:56 +0800

    完成本地测试，不懂为什么自动保存后format会改变汇编文件的缩进

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -5,20 +5,35 @@ n: .word 8
 
 .text
 main:
-    la t0, n
-    lw a0, 0(t0)
-    jal ra, factorial
+	la t0, n
+lw a0, 0(t0)
+	add t0, x0, a0
+	jal ra, factorial
 
-    addi a1, a0, 0
-    addi a0, x0, 1
-    ecall # Print Result
+	addi a1, a0, 0
+	addi a0, x0, 1
+	ecall # Print Result
 
-    addi a1, x0, '\n'
-    addi a0, x0, 11
-    ecall # Print newline
+	addi a1, x0, '\n'
+	addi a0, x0, 11
+	ecall # Print newline
 
-    addi a0, x0, 10
-    ecall # Exit
+	addi a0, x0, 10
+	ecall # Exit
 
-factorial:
-    # YOUR CODE HERE
+	factorial:
+	addi sp, sp, -8
+	sw a0, 4(sp)
+sw ra, 0(sp)
+	addi a0, a0, -1
+	bne t0, x0, L1
+	addi a0, x0, 1
+	jr ra
+	L1:
+	jal ra, factorial
+	add t0, x0, a0
+	lw ra, 0(sp)
+lw a0, 4(sp)
+	addi sp, sp, 8
+	mul a0, a0, t0
+	jr ra
