84b0e725c7dc5deda38ae06a3f5d8ca97e376f53
Pluto-yubin <1113940328@qq.com>
2022-04-24 20:48:37 +0800

    finish task 2

--- su20-proj4-starter/setup.py
+++ su20-proj4-starter/setup.py
@@ -6,7 +6,16 @@ def main():
     LDFLAGS = ['-fopenmp']
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
-    # TODO: YOUR CODE HERE
+     module1 = Extension('numc',
+                        sources=['numc.c', 'matrix.c'],
+                        include_dirs=['/usr/include/python3.8'],
+                        extra_compile_args=CFLAGS)
+    setup(
+        name='numc',
+        version='1.0',
+        description='This is cs61c su20 proj 4 task 2',
+        ext_modules=[module1]
+    )
 
 if __name__ == "__main__":
     main()
