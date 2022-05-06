7cb0e5055dee55f7084c993ac800e85d54eb6830
Pluto-yubin <1113940328@qq.com>
2022-05-06 20:32:27 +0800

    to lab10 exercise3 part2

--- lab10/dotp.c
+++ lab10/dotp.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int array_size = 10000000;
+int array_size = 100000;
 
 #include "omp_apps.h"
 
-int main() {
-  char *report = compute_dotp(array_size);
-  printf("%s\n", report);
-  return 0;
+int main()
+{
+    char* report = compute_dotp(array_size);
+    printf("%s\n", report);
+    return 0;
 }
